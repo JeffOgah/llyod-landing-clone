519a84fc7b61620d9cacf41485187da3004bd6c9
JeffOgah <jeff.ogah@gmail.com>
2019-06-18 21:41:30 +0100

    Page updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,15 @@
 function share() {
     const toggle = document.getElementById("share");
     const btnShare = document.getElementById("btn-share");
-    const dark = "#141414";
-    const white = "white";
-    const light = "#f1f1f1"
 
     if (toggle.style.display !== "block") {
         toggle.style.display = "block";
-        btnShare.style.backgroundColor = dark;
-        btnShare.style.color = white;
+        btnShare.style.backgroundColor = "#141414";
+        btnShare.style.color = "white";
     } 
     else {
         toggle.style.display = "none";
-        btnShare.style.backgroundColor = light;
-        btnShare.style.color = dark;
+        btnShare.style.backgroundColor = "#f1f1f1";
+        btnShare.style.color = "#0c4ab5";
     }
 }
